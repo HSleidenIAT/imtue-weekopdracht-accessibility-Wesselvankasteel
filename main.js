26317b7fce8f67ac9a729975f923db0beda2bd36
Wessel <wesselvankasteel@ziggo.nl>
2020-01-24 20:37:01 +0100

    Accessibility toegepast op site

--- main.js
+++ main.js
@@ -5,9 +5,9 @@ const commentWrapper = document.querySelector('.comment-wrapper');
 
 commentWrapper.style.display = 'none';
 
-showHideBtn.onclick = function() {
+showHideBtn.onclick = function () {
   let showHideText = showHideBtn.textContent;
-  if(showHideText === 'Show comments') {
+  if (showHideText === 'Show comments') {
     showHideBtn.textContent = 'Hide comments';
     commentWrapper.style.display = 'block';
   } else {
@@ -23,7 +23,7 @@ const nameField = document.querySelector('#name');
 const commentField = document.querySelector('#comment');
 const list = document.querySelector('.comment-container');
 
-form.onsubmit = function(e) {
+form.onsubmit = function (e) {
   e.preventDefault();
   submitComment();
 };
